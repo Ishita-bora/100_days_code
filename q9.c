810e753db831e66d78999787ab5b817c808cd69b
ishita bora <ishitabora20@gmail.com>
2025-11-01 13:28:58 +0530

    program for interest calculation
    Added a comment to describe the program's purpose.

--- q9.c
+++ q9.c
@@ -1,3 +1,6 @@
+Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+    
 #include <stdio.h>
 int main() {
     double principal, rate, time, simple_interest;
