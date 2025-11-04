8366cf87d99765750bbb5bfe1b1f6ec9fbd2d20a
ishita bora <ishitabora20@gmail.com>
2025-11-04 09:43:28 +0530

    sum calculation for odd numbers
    Calculate the sum of odd numbers up to n.

--- q27.c
+++ q27.c
@@ -3,14 +3,17 @@ Write a program to print the sum of the first n odd numbers.
     
 #include <stdio.h>
 int main() {
-    int n, i;
+    int n, i, sum=0;
     printf("Enter a number: ");
     scanf("%d", &n);
     for(i=1; i<=n; i++)
     {
         if (i % 2 != 0) {
-            printf("%d ", i);
+            sum += i;
         }
+            
+        
     }
+    printf("%d ", sum);
     return 0;
 }
