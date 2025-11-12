b62281d1d436f32f16223be6f59bccb0dc1e5af5
ishita bora <ishitabora20@gmail.com>
2025-11-12 19:51:22 +0530

    Calculate product of odd digits in a number

--- q39.c
+++ q39.c
@@ -1,14 +1,16 @@
+Write a program to find the product of odd digits of a number.
+
 #include <stdio.h>
 int main()
 {
-    int n, digit, sum = 0;
+    int n, digit, product = 1;
     printf("Enter a number: ");
     scanf("%d", &n);
     while (n != 0 && n % 2 != 0) {
         digit = n % 10;
-        sum += digit;
+        product *= digit;
         n /= 10;
     }
-    printf("Sum of odd digits: %d\n", sum);
+    printf("Product of odd digits: %d\n", product);
     return 0;
 }
